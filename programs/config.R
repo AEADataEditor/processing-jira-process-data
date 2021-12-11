b543e9284775a64bdbe181ae7bfab7785a9cdfbe
Lars Vilhuber <lars@vilhuber.com>
2021-12-11 16:26:28 -0500

    Correcting bug, updating extract date

--- programs/config.R
+++ programs/config.R
@@ -8,11 +8,11 @@ download_raw <- TRUE
 
 ## This pins the date of the to-be-processed file
 
-extractday <- "11-22-2021"
+extractday <- "12-09-2021"
 
 ## These define the start (and end) dates for processing of data
 firstday <- "2020-12-01"
-lastday  <- "2020-22-11"
+lastday  <- "2021-11-30"
 
 # ###########################
 # CONFIG: define paths and filenames for later reference
@@ -27,6 +27,11 @@ setwd(basepath)
 
 # for Jira stuff
 jiraconf <- file.path(basepath,"data","confidential")
+
+# for local processing
+if ( Sys.getenv("HOSTNAME") == "zotique3" ) {
+  jirconf <- paste0(Sys.getenv("XDG_RUNTIME_DIR"),"/gvfs/dav:host=dav.box.com,ssl=true/dav/Office of AEA Data Editor/InternalData")
+}
 jiraanon <- file.path(basepath,"data","anon")
 jirameta <- file.path(basepath,"data","metadata")
 
