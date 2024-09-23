15a969511d56ecc35d0a887df1ea9ddfecf64321
Takshil Sachdev <144289677+ts724@users.noreply.github.com>
2024-09-23 17:45:35 +0000

    full check

--- programs/config.R
+++ programs/config.R
@@ -8,7 +8,7 @@ download_raw <- TRUE
 
 ## This pins the date of the to-be-processed file
 
-extractday <- "2024-09-02"
+extractday <- "2024-09-23"
 
 ## These define the start (and end) dates for processing of data
 firstday <- "2023-12-01"
